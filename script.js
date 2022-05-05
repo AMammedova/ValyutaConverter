8b9cfaff7966315c95ecf9df7b904e776fa6abf9
AMammedova <ayselmemmedova1718@gmail.com>
2022-05-05 14:48:36 +0400

    changes

--- script.js
+++ script.js
@@ -40,14 +40,29 @@ toInp.addEventListener("keyup",()=>{
 })
 
 function Convert(){
-    fetch(`https://api.exchangerate.host/latest?base=${fromValue}&symbols=${toValue}`).then(res=>res.json()).then(data=>{
+  if(fromValue==toValue){
+    fromInp.value!=null?toInp.value=fromInp.value:fromInp.value=toInp.value
+    moneyTextFrom.innerText=`1 ${toValue}= 1 ${fromValue}`
+    moneyTextTo.innerText=`1 ${toValue}= 1 ${fromValue}`
+    console.log("istek getmedi")
        
+  }
+  
+  else{
+    fetch(`https://api.exchangerate.host/latest?base=${fromValue}&symbols=${toValue}`).then(res=>res.json()).then(data=>{
+       console.log("istek getdi")
        
         moneyTextFrom.innerText=` 1 ${fromValue} =${data.rates[toValue]} ${toValue}`
         toInp.value= `${(fromInp.value*data.rates[toValue]).toFixed(2)}`
         moneyTextTo.innerText=`1 ${toValue}=${1/data.rates[toValue]} ${fromValue}`
   
   })
+  .catch((error)=>{
+    console.log(error)
+  })
+  }
+
+    
 }
 reverse.addEventListener("click",()=>{
   
@@ -65,13 +80,27 @@ reverse.addEventListener("click",()=>{
 })
 
 function Converto(){
+  
+  if(fromValue==toValue){
+    fromInp.value!=null?toInp.value=fromInp.value:fromInp.value=toInp.value
+    moneyTextFrom.innerText=`1 ${toValue}= 1 ${fromValue}`
+    moneyTextTo.innerText=`1 ${toValue}= 1 ${fromValue}`
+    console.log("istek getmedi")
+       
+  }
+  
+  else{
     fetch(`https://api.exchangerate.host/latest?base=${toValue}&symbols=${fromValue}`).then(res=>res.json()).then(data=>{
        
 
         moneyTextTo.innerText=` 1 ${toValue} =${data.rates[fromValue]} ${fromValue}`
-        fromInp.value= `${toInp.value*data.rates[fromValue]}`
+        fromInp.value= `${(toInp.value*data.rates[fromValue]).toFixed(2)}`
         moneyTextFrom.innerText=`1 ${fromValue}=${1/data.rates[fromValue]} ${toValue}`
   
   })
+  .catch((error)=>{
+    console.log(error)
+  })
+  }
 }
  
